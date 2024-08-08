f5fc72721040c0208db72c7096d3239c0f76a2bf
FemputerTech <mleicht92@gmail.com>
2024-08-08 12:24:06 -0700

    setup spotify api

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from dashboard import create_app
-import os
 
-SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
 app = create_app()
 
+
 if __name__ == '__main__':
+    print({"details":"It's aliiiiive!!"})
     app.run(host='0.0.0.0', port=5000, debug=True)
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,46 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import requests
+import base64
+import os
+
 
 views = Blueprint("views", __name__)
 
+
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+
+def get_access_token(client_id, client_secret):
+    auth_url = 'https://accounts.spotify.com/api/token'
+    headers = {
+        'Authorization': 'Basic ' + base64.b64encode(f'{client_id}:{client_secret}'.encode()).decode(),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    data = {
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(auth_url, headers=headers, data=data)
+    response_data = response.json()
+    return response_data['access_token']
+
+
+def search_band(access_token, band_name='Ween'):
+    search_url = 'https://api.spotify.com/v1/search'
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    params = {
+        'q': band_name,
+        'type': 'artist',
+        'limit': 1
+    }
+    response = requests.get(search_url, headers=headers, params=params)
+    return response.json()
+
+
 @views.route("/", methods=["GET"])
 def index():
-    return {"details":"It's aliiiiive!!"}
+    token = get_access_token(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET)
+    data = search_band(token)
+    return jsonify(data)
